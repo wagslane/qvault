25ba78fe93e1f94a422e9de8d146f8b4aa3e9391
lane-c-wagner <lane.c.wagner@gmail.com>
2019-01-17 14:51:57 -0700

    mocha

--- test/QVaultCrypto.test.js
+++ test/QVaultCrypto.test.js
@@ -7,28 +7,29 @@ import {
   HashDoorKey,
   GetMasterKeyFromCardMnemonic,
   GetMasterKeyFromCardQR
-} from './QVaultCrypto';
+} from '../src/lib/QVaultCrypto/QVaultCrypto';
+import { expect } from 'chai';
 
 it('deterministic master hashes', async () => {
   const hash1 = await HashMasterKey('lanewagner');
   const hash2 = await HashMasterKey('lanewagner');
   const hash3 = await HashMasterKey('lanewagners');
-  expect(hash1).toEqual(hash2);
-  expect(hash1).not.toEqual(hash3);
+  expect(hash1).equal(hash2);
+  expect(hash1).not.equal(hash3);
 });
 
 it('deterministic door hashes', async () => {
   const hash1 = await HashDoorKey('lanewagner', 1234);
   const hash2 = await HashDoorKey('lanewagner', 1234);
   const hash3 = await HashDoorKey('lanewagners', 5555);
-  expect(hash1).toEqual(hash2);
-  expect(hash1).not.toEqual(hash3);
+  expect(hash1).equal(hash2);
+  expect(hash1).not.equal(hash3);
 });
 
 it('unique keys', () => {
   const key1 = GetNewMasterKey();
   const key2 = GetNewMasterKey();
-  expect(key1).not.toEqual(key2);
+  expect(key1).not.equal(key2);
 });
 
 it('cipher and decipher', async () => {
@@ -40,17 +41,19 @@ it('cipher and decipher', async () => {
   const hashedMasterKey = await HashMasterKey(GetNewMasterKey());
   const ciphered = CipherSecret(hashedMasterKey, mySecret);
   const deciphered = DecipherSecret(hashedMasterKey, ciphered);
-  expect(mySecret).toEqual(deciphered);
+  expect(mySecret.coin).equal(deciphered.coin);
+  expect(mySecret.mnemonic).equal(deciphered.mnemonic);
+  expect(mySecret.description).equal(deciphered.description);
 });
 
 it('validate password', () => {
   // truthy = "not valid" because the messages mean there was an error
-  expect(ValidatePassword('somewordthatslong')).toBeTruthy();
-  expect(ValidatePassword('somewordthatslong1')).toBeTruthy();
-  expect(ValidatePassword('somewordthatslong@1')).toBeTruthy();
-  expect(ValidatePassword('lonG@1')).toBeTruthy();
+  expect(ValidatePassword('somewordthatslong')).not.equal('');
+  expect(ValidatePassword('somewordthatslong1')).not.equal('');
+  expect(ValidatePassword('somewordthatslong@1')).not.equal('');
+  expect(ValidatePassword('lonG@1')).not.equal('');
 
-  expect(ValidatePassword('somewordthatslonG@1')).toBeFalsy();
+  expect(ValidatePassword('somewordthatslonG@1')).equal('');
 });
 
 it('GetMasterKeyFromCardMnemonicSuccess', () => {
@@ -72,8 +75,8 @@ it('GetMasterKeyFromCardMnemonicSuccess', () => {
     '16UjcYNBG9'
   );
   const randomMasterKey = GetNewMasterKey();
-  expect(randomMasterKey.length).toEqual(masterKey.length);
-  expect(Buffer.from(masterKey, 'base64').length).toEqual(24);
+  expect(randomMasterKey.length).equal(masterKey.length);
+  expect(Buffer.from(masterKey, 'base64').length).equal(24);
 });
 
 it('GetMasterKeyFromCardMnemonicFail', () => {
@@ -111,21 +114,21 @@ it('GetMasterKeyFromCardMnemonicFail', () => {
     '16UjcYNBG'
   );
   const randomMasterKey = GetNewMasterKey();
-  expect(randomMasterKey.length).not.toEqual(masterKeyBadMnemonic.length);
-  expect(randomMasterKey.length).not.toEqual(masterKeyBadPrivateCode.length);
+  expect(randomMasterKey.length).not.equal(masterKeyBadMnemonic.length);
+  expect(randomMasterKey.length).not.equal(masterKeyBadPrivateCode.length);
 });
 
 it('GetMasterKeyFromCardQRSuccess', () => {
   const masterKey = GetMasterKeyFromCardQR('Y6Z46buO2GYUDdSoab6j1A==', '16UjcYNBG9');
   const randomMasterKey = GetNewMasterKey();
-  expect(randomMasterKey.length).toEqual(masterKey.length);
-  expect(Buffer.from(masterKey, 'base64').length).toEqual(24);
+  expect(randomMasterKey.length).equal(masterKey.length);
+  expect(Buffer.from(masterKey, 'base64').length).equal(24);
 });
 
 it('GetMasterKeyFromCardQRFail', () => {
   const masterKeyBadMnemonic = GetMasterKeyFromCardQR('Y6Z46buO2GYUDdSoab6j1==', '16UjcYNBG9');
   const masterKeyBadPrivateCode = GetMasterKeyFromCardQR('Y6Z46buO2GYUDdSoab6j1A==', '16UjcYNBG');
   const randomMasterKey = GetNewMasterKey();
-  expect(randomMasterKey.length).not.toEqual(masterKeyBadMnemonic.length);
-  expect(randomMasterKey.length).not.toEqual(masterKeyBadPrivateCode.length);
+  expect(randomMasterKey.length).not.equal(masterKeyBadMnemonic.length);
+  expect(randomMasterKey.length).not.equal(masterKeyBadPrivateCode.length);
 });
