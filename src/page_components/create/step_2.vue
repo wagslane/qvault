2f02d6bd733ec9b8c285eb69fedc9f5552e3587f
Preston <pwag42@hotmail.com>
2019-01-19 19:03:50 -0700

    no need to say it twice

--- src/page_components/create/step_2.vue
+++ src/page_components/create/step_2.vue
@@ -1,8 +1,8 @@
 <template>
   <form @submit.prevent="save_step_2">
-    <div v-if="char_key">
+    <div v-if="$root.char_key">
       <h2>Write the following characters on your Q-Card:</h2>
-      <h3>{{char_key}}</h3>
+      <h3>{{$root.char_key}}</h3>
       <button type="submit">Next</button>
     </div>
     <div v-else>
@@ -15,20 +15,14 @@
   import {GenerateCharKey} from '../../lib/QVaultCrypto/QVaultCrypto';
 
   export default {
-    data(){
-      return {
-        char_key: null,
-      }
-    },
     mounted(){
       this.generate_key();
     },
     methods:{
       async generate_key(){
-        this.$root.char_key = this.char_key = await GenerateCharKey();
+        this.$root.char_key = await GenerateCharKey();
       },
       save_step_2(){
-        this.$root.char_key = this.char_key;
         this.$router.push({name: 'create_step_3'})
       },
     },
