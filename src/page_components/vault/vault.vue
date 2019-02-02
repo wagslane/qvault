1a9e2cfbe2c516e7563ed9ed684f03e2df19bc61
Preston <pwag42@hotmail.com>
2019-02-02 14:42:37 -0700

    choose between local and cloud vault

---
+++ src/page_components/vault/vault.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div class="sidebar">
+      <img src="../../img/qvault-logo.png">
+
+    </div>
+    <div class="content">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
