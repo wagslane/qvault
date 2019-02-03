5aa0ab5891dcc0dce87dc36bc06f9c05ce8d3963
lane-c-wagner <lane.c.wagner@gmail.com>
2019-02-03 10:26:20 -0700

    test deploy

--- scripts/deploy.travis.sh
+++ scripts/deploy.travis.sh
@@ -1,3 +1,2 @@
-[ "$TRAVIS_OS_NAME" = linux ] && docker run --rm -e GH_TOKEN -v ${PWD}:/project -v ~/.cache/electron:/root/.cache/electron -v ~/.cache/electron-builder:/root/.cache/electron-builder electronuserland/builder:wine /bin/bash -c "yarn --link-duplicates --pure-lockfile && yarn release --linux --win"
-
 [ "$TRAVIS_OS_NAME" = "mac os x" ] && yarn release
+[ "$TRAVIS_OS_NAME" = linux ] && docker run --rm -e GH_TOKEN -v ${PWD}:/project -v ~/.cache/electron:/root/.cache/electron -v ~/.cache/electron-builder:/root/.cache/electron-builder electronuserland/builder:wine /bin/bash -c "yarn --link-duplicates --pure-lockfile && yarn release --linux --win"
