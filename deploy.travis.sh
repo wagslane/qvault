174886231d29791fdea321269a52a750f7289121
lane-c-wagner <lane.c.wagner@gmail.com>
2019-02-03 11:12:35 -0700

    fix mac deploy

--- deploy.travis.sh
+++ deploy.travis.sh
@@ -1,2 +1,3 @@
-[ "$TRAVIS_OS_NAME" = osx ] && yarn release
+#! /bin/bash
+[ "$TRAVIS_OS_NAME" = osx ] && npx webpack --progress --config webpack.config.js --env=production && ./node_modules/.bin/electron-builder
 [ "$TRAVIS_OS_NAME" = linux ] && docker run --rm -e GH_TOKEN -v ${PWD}:/project -v ~/.cache/electron:/root/.cache/electron -v ~/.cache/electron-builder:/root/.cache/electron-builder electronuserland/builder:wine /bin/bash -c "yarn --link-duplicates --pure-lockfile && yarn release --linux --win"
