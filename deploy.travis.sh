a34806b863698deb78bf5a0d704512676743d6e4
lane-c-wagner <lane.c.wagner@gmail.com>
2019-02-03 17:13:02 -0700

    node mods

--- deploy.travis.sh
+++ deploy.travis.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-[ "$TRAVIS_OS_NAME" = osx ] && yarn release && exit 0
+[ "$TRAVIS_OS_NAME" = osx ] && yarn release
 [ "$TRAVIS_OS_NAME" = linux ] && docker run --rm -e GH_TOKEN -v ${PWD}:/project -v ~/.cache/electron:/root/.cache/electron -v ~/.cache/electron-builder:/root/.cache/electron-builder electronuserland/builder:wine /bin/bash -c "yarn --link-duplicates --pure-lockfile && yarn release --linux --win"
