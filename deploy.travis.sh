4667a845d10fb09bb88f5b3dace808912c8c9866
lane-c-wagner <lane.c.wagner@gmail.com>
2019-02-04 09:32:55 -0700

    if statement

--- deploy.travis.sh
+++ deploy.travis.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
-[ "$TRAVIS_OS_NAME" = osx ] && yarn release && exit 0
-[ "$TRAVIS_OS_NAME" = linux ] && docker run --rm -e GH_TOKEN -v ${PWD}:/project -v ~/.cache/electron:/root/.cache/electron -v ~/.cache/electron-builder:/root/.cache/electron-builder electronuserland/builder:wine /bin/bash -c "yarn --link-duplicates --pure-lockfile && yarn release --linux --win" 
+if [ "$TRAVIS_OS_NAME" == osx ]; then
+    yarn release
+else
+    docker run --rm -e GH_TOKEN -v "${PWD}":/project -v ~/.cache/electron:/root/.cache/electron -v ~/.cache/electron-builder:/root/.cache/electron-builder electronuserland/builder:wine /bin/bash -c "yarn --link-duplicates --pure-lockfile && yarn release --linux --win"
+fi
