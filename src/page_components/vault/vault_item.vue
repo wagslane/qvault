a8b2899bd6eca9d0f4e1215cc687a7fea4afc0fe
Preston <pwag42@hotmail.com>
2019-02-05 19:31:58 -0700

    fleshing out vault

--- src/page_components/vault/vault_item.vue
+++ src/page_components/vault/vault_item.vue
@@ -3,3 +3,16 @@
 
   </form>
 </template>
+
+<script>
+  export default {
+    computed: {
+      uuid(){ return this.$route.params.secret_uuid; },
+      secret(){
+        if(this.$root.secrets && this.uuid){
+          return this.$root.secrets[this.uuid];
+        }
+      },
+    }
+  }
+</script>
