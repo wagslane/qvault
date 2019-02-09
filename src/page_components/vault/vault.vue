8fdb246677b5e5e51d17782ac69541b6cc37b813
Preston <pwag42@hotmail.com>
2019-02-09 13:28:34 -0700

    added save button to vault, more styling, etc

--- src/page_components/vault/vault.vue
+++ src/page_components/vault/vault.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="container">
-    <div class="sidebar">
+    <div class="sidebar" v-if="$root.secrets">
       <input
         type="text"
         class="search"
         placeholder="search"
       >
-      <router-link
-        v-for="(box, uuid) in $root.secrets"
-        :key="uuid"
-        :to="{name: 'vault_item', params: {box_uuid: uuid}}"
-      >
-        {{box.name}}
-      </router-link>
+      <div class="boxes">
+        <router-link
+          v-for="(box, uuid) in $root.secrets"
+          :key="uuid"
+          :to="{name: 'vault_item', params: {secret_uuid: uuid}}"
+          class="box_link"
+        >
+          {{box.name}}
+        </router-link>
+      </div>
       <button
         @click.prevent="add_box"
         class="add_box"
@@ -40,3 +43,60 @@
     },
   }
 </script>
+
+<style lang="less" scoped>
+  .container {
+    display: flex; /* or inline-flex */
+    flex-direction: row;
+    height: 100vh;
+
+    .sidebar {
+      width: 25%;
+      padding: 15px;
+      background-color: #32373B;
+      height: 100vh;
+    }
+
+    .content {
+      width: 75%;
+    }
+  }
+
+  .boxes {
+    height: ~'calc(100vh - 174px)';
+    overflow-y: auto;
+    margin-top: 15px;
+    margin-bottom: 15px;
+
+    .box_link {
+      color: white;
+      padding: 22px;
+      display: block;
+
+      &.router-link-active {
+        color: #D8A22E;
+      }
+
+      &:nth-child(2n){
+        background-color: #181C1E;
+      }
+    }
+  }
+
+  .search {
+    width: 100%;
+    border-radius: 6px;
+    background-color: #080D0E;
+    color: white;
+    padding: 10px;
+  }
+
+  .add_box {
+    width: 100%;
+    height: 75px;
+    text-align: center;
+    bottom: 100%;
+    background-color: #181C1E;
+    color: white;
+  }
+</style>
--- src/page_components/vault/vault_item.vue
+++ src/page_components/vault/vault_item.vue
@@ -1,13 +1,14 @@
 <template>
-  <form class="wrapper">
+  <form class="wrapper" v-if="box">
     <input v-model="box.name" placeholder="Name">
     <button @click.prevent="add_secret">Add</button>
     <hr />
-    <div v-for="secret in box.secrets">
+    <div v-for="secret in box.secrets" class="secret">
       <input placeholder="name" v-model="secret.name">
       <input placeholder="value" v-model="secret.value">
-      <button v-clipboard:copy="secret.value">copy</button>
+      <!--<button v-clipboard:copy="secret.value">copy</button>-->
     </div>
+    <button @click.prevent="$root.SaveLocalVault()">Save</button>
   </form>
 </template>
 
@@ -16,13 +17,37 @@
     computed: {
       uuid(){ return this.$route.params.secret_uuid; },
       box(){
-        return this.$root.GetSecret(this.uuid);
+        if(this.uuid){
+          return this.$root.GetSecret(this.uuid);
+        }
       },
     },
     methods: {
-      copy_value(){
-        document.execCommand("copy");
+//      copy_value(){
+//        document.execCommand("copy");
+//      },
+      add_secret(){
+        this.box.secrets.push({
+          name: '',
+          value: '',
+        })
       },
     },
   }
 </script>
+
+<style lang="less" scoped>
+  .wrapper {
+    border-radius: 6px;
+    background-color: #080D0E;
+    margin: 25px;
+    padding: 25px;
+  }
+
+  hr {
+    box-sizing: border-box;
+    height: 2px;
+    width: 728px;
+    border: 1px solid #2F3235;
+  }
+</style>
