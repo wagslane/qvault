63fd89ad3970febbfc70dc2ce27b7c5f8f342208
lane-c-wagner <lane.c.wagner@gmail.com>
2019-02-18 12:55:35 -0700

    QR code and then password

--- src/page_components/load/unlock.vue
+++ src/page_components/load/unlock.vue
@@ -5,6 +5,7 @@
       <div class="body">
         <h1>Unlock Vault</h1>
         <form v-if="!qrRequired" @submit.prevent="unlock">
+          <h2>Please enter your password or passphrase</h2>
           <TextInput
             v-model="password"
             :active="true"
@@ -17,7 +18,10 @@
             type="submit"
           >Unlock</button>
         </form>
-        <QRScanner v-if="qrRequired" @scanned="handleQRKey"  />
+        <div v-if="qrRequired">
+          <h2>Please scan your Q Card</h2>
+          <QRScanner @scanned="handleQRKey" />
+        </div>
       </div>
       <div class="footer">
         <div class="back" @click="$router.go(-1)">
@@ -40,12 +44,13 @@
         qrRequired: false
       }
     },
+    mounted: function(){
+      if (this.$root.loaded_vault.qr_required){
+        this.qrRequired = true
+      }
+    },
     methods: {
       async unlock(){
-        if (this.$root.loaded_vault.qr_required){
-          this.qrRequired = true
-          return
-        }
         try{
           await this.$root.UnlockVaultPassword(this.password)
           this.$router.push({name: 'vault'});
@@ -55,18 +60,21 @@
       },
       async handleQRKey(qrKey) {
         if (qrKey.substring(0, 6) === 'ERROR:'){
-          this.error = qrKey
+          this.error = qrKey;
           return
         }
         if (!ValidateQRKey(qrKey)){
-          this.error = `Not a valid QR key`
+          this.error = `Not a valid QR key`;
           return
         }
         this.$root.CreateQrKey(qrKey);
-        await this.$root.UnlockVaultQr(qrKey)
-        await this.$root.UnlockVaultPassword(this.password)
-        this.$router.push({name: 'vault'});
+        await this.$root.UnlockVaultQr(qrKey);
+        this.qrRequired = false;
+        this.error = '';
       }
     },
+    components:{
+      QRScanner,
+    }
   }
 </script>
