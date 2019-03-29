1d7a2525a0855da545b5d94f7b24e7f207a986e9
lane-c-wagner <lane.c.wagner@gmail.com>
2019-03-29 14:47:04 -0600

    must return something

--- src/page_components/vault/secret.vue
+++ src/page_components/vault/secret.vue
@@ -57,36 +57,37 @@ import Vue from 'vue';
 
 import box_types from '../../consts/box_types.es6';
 
-  export default {
-    computed: {
-      secret_uuid(){ return this.$route.params.secret_uuid; },
-      box(){ return this.$parent.box; },
-      secret(){
-        return this.box.secrets[this.secret_uuid];
-      },
-      box_type(){
-        return box_types.find(box_type => box_type.name === this.box.type);
-      },
-      fields(){
-        if(this.box_type){
-          return this.box_type.fields;
-        }
-      },
+export default {
+  computed: {
+    secret_uuid(){ return this.$route.params.secret_uuid; },
+    box(){ return this.$parent.box; },
+    secret(){
+      return this.box.secrets[this.secret_uuid];
     },
-    methods: {
-      add_to_sublist(field){
-        let new_value = {};
-        for(let subfield of field.subfields){
-          let value = null;
-          if(subfield.type === Array){
-            value = [];
-          }
-          Vue.set(new_value, subfield.name, value);
-        }
-        this.secret[field.name].push(new_value);
-      },
+    box_type(){
+      return box_types.find(box_type => box_type.name === this.box.type);
     },
-  };
+    fields(){
+      if(this.box_type){
+        return this.box_type.fields;
+      }
+      return {};
+    },
+  },
+  methods: {
+    add_to_sublist(field){
+      let new_value = {};
+      for(let subfield of field.subfields){
+        let value = null;
+        if(subfield.type === Array){
+          value = [];
+        }
+        Vue.set(new_value, subfield.name, value);
+      }
+      this.secret[field.name].push(new_value);
+    },
+  },
+};
 </script>
 
 <style lang="less" scoped>
