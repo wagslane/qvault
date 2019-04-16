5ce109f323ccb674b3ccfcb6f3f400fd951e6438
lane-c-wagner <lane.c.wagner@gmail.com>
2019-04-16 16:44:54 -0600

    testing auto update

--- main.js
+++ main.js
@@ -53,6 +53,7 @@ function createWindow() {
     // Dereference the window object
     mainWindow = null;
   });
+  return mainWindow;
 }
 
 autoUpdater.on('update-available', (info) => {
@@ -74,24 +75,16 @@ autoUpdater.on('update-downloaded', (info) => {
   sendStatusToWindow('Update downloaded. ' + info);
 });
 
-app.on('ready', createWindow);
-
 app.on('ready', function () {
+  createWindow();
   autoUpdater.checkForUpdatesAndNotify();
 });
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+  app.quit();
 });
 
 app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
     createWindow();
   }
