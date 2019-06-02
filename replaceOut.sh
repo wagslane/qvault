7920c2cb8a5a3118eeab9bd041bda744ef10e37f
lane-c-wagner <lane.c.wagner@gmail.com>
2019-06-01 18:54:38 -0600

    fix typo

--- replaceOut.sh
+++ replaceOut.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 mv node_modules/app-builder-lib/out appBuilderLibTemp
 rm -rf node_modules/app-builder-lib/out
-cp -R replaceOut node_modules/app-builder-lib/out
+cp -R appBuilderLibKilian node_modules/app-builder-lib/out
 
 mv node_modules/dmg-builder/out dmgBuilderTemp
 rm -rf node_modules/dmg-builder/out
-cp -R replaceOut node_modules/dmg-builder/out
+cp -R dmgBuilderKilian node_modules/dmg-builder/out
