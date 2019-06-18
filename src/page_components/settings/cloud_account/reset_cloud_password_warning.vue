1fed99b58dc39d7a16f8640fa72eeb826d458855
lwagner <lwagner@solutionreach.com>
2019-06-18 16:04:50 -0600

    temp

---
+++ src/page_components/settings/cloud_account/reset_cloud_password_warning.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <HeaderBar title="Settings" />
+    <div class="options-box">
+      <form @submit.prevent="$router.push({name: 'utility_reset_cloud_password'});">
+        <div class="body center-text">
+          <h1>Are You Sure?</h1>
+          <h2>If you reset your cloud account password your existing vault will be overwritten</h2>
+        </div>
+        <div class="footer">
+          <div
+            class="back"
+            @click="$router.go(-1)"
+          >
+            <div class="icon" />
+          </div>
+
+          <button
+            class="continue"
+            type="submit"
+          >
+            <div class="continue-arrow" />
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
