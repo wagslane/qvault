361eecdd0548c35b48cc779e044be6c4ec2900fe
lwagner <lwagner@solutionreach.com>
2019-08-09 15:34:01 -0600

    view char key

---
+++ src/pageComponents/settings/charKey/Choose.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <HeaderBar title="Settings" />
+    <div class="options-box">
+      <div class="body center-text">
+        <h1>Manage Recovery Card</h1>
+        <h2>View or change the recovery code for your vault</h2>
+
+        <div
+          class="btn"
+          @click="showViewRecoveryCodeModal"
+        >
+          View Recovery Code
+        </div>
+
+        <router-link
+          class="btn"
+          :to="{name: 'settingsCharKeyChange'}"
+        >
+          Change Recovery Code
+        </router-link>
+      </div>
+      <div class="footer">
+        <div
+          class="back"
+          @click="$router.push({name: 'settings'})"
+        >
+          <div class="icon" />
+        </div>
+      </div>
+    </div>
+    <confirm
+      ref="viewRecoveryCodeModal"
+      title="View Recovery Code"
+      subtitle="Are you sure you want to see your recovery code? Make sure no one is around to see it."
+    />
+  </div>
+</template>
+
+<script>
+import confirm from '../../../components/Confirm.vue';
+
+export default {
+  components:{
+    confirm
+  },
+  methods:{
+    viewRecoveryCode(){
+      this.$router.push({name: 'settingsCharKeyView'});
+    },
+    showViewRecoveryCodeModal(){
+      this.$refs.viewRecoveryCodeModal.show(this.viewRecoveryCode);
+    },
+  },
+};
+</script>
---
+++ src/pageComponents/settings/charKey/View.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <HeaderBar title="Settings" />
+    <div class="options-box">
+      <div class="body">
+        <h1>Current Recovery Code</h1>
+        <h2>
+          This recovery code can be used to reset your password if you forget it.
+        </h2>
+
+        <div class="highlight-box">
+          <span class="title">This is your current recovery code. Keep it in a safe place. </span>
+          <div class="flex">
+            <div
+              v-for="(word, i) in split"
+              :key="i"
+              class="character-code"
+            >
+              <span
+                v-for="(char, j) in word"
+                :key="j"
+                class="spacing"
+              >
+                <u v-if="/[A-Z]/.test(char)">{{ char }}</u>
+                <span v-else>{{ char }}</span>
+              </span>
+            </div>
+          </div>
+        </div>
+        <h3> The code is case-sensitive, underlines are used for capital letters </h3>
+
+        <br>
+        <br>
+      </div>
+      <div class="footer">
+        <div
+          class="back"
+          @click="$router.go(-1)"
+        >
+          <div class="icon" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed:{
+    split(){
+      return [
+        this.$root.char_key.slice(0, 4),
+        this.$root.char_key.slice(4, 8),
+        this.$root.char_key.slice(8, 12),
+        this.$root.char_key.slice(12, 16)
+      ];
+    }
+  },
+};
+</script>
