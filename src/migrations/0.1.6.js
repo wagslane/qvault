254bf7dbfdc834462087f468f68565f579feec3d
Preston <pwag42@hotmail.com>
2019-08-12 14:50:37 -0600

    camelCase

--- src/migrations/0.1.6.js
+++ src/migrations/0.1.6.js
@@ -1,16 +1,16 @@
-export default function(loaded_vault, decrypted_secrets){
-  for(let box of Object.values(decrypted_secrets)){
+export default function(loadedVault, decryptedSecrets){
+  for(let box of Object.values(decryptedSecrets)){
     for(let secret of Object.values(box.secrets)) {
       let fields = {};
-      for (let field_key of Object.keys(secret)){
-        if (field_key !== 'created') {
-          fields[ field_key ] = secret[ field_key ];
-          delete secret[ field_key ];
+      for (let fieldKey of Object.keys(secret)){
+        if (fieldKey !== 'created') {
+          fields[ fieldKey ] = secret[ fieldKey ];
+          delete secret[ fieldKey ];
         }
         secret.fields = fields;
       }
     }
   }
-  loaded_vault.version = '0.2.0';
-  return [ loaded_vault, decrypted_secrets ];
+  loadedVault.version = '0.2.0';
+  return [ loadedVault, decryptedSecrets ];
 }
