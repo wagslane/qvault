2371a87a7da24e80efd1b1efbe6e2ec1ce74c904
Elena Mikhaylova <Lena@Mac.lan>
2020-01-04 11:23:21 -0500

    create and modify preferences.json file for online preference

---
+++ src/lib/appPreferences.js
@@ -0,0 +1,34 @@
+export function getPreferenceFromFile(key){
+  if (!preferenceFileExists()){
+    return;
+  }
+  const preferences = getPreferences(getPreferenceFilePath());
+  return preferences[key]; 
+}
+
+export function writeToPreferenceFile(key, value){
+  const path = getPreferenceFilePath();
+  const preferences = getPreferences(path);
+  preferences[key] = value;
+  window.nodeAPI.fs.writeFileSync(path, JSON.stringify(preferences));
+}
+
+export function preferenceFileExists () {
+  return window.nodeAPI.fs.existsSync(getPreferenceFilePath());
+}
+
+export function createPreferenceFile () {
+  const defaultPreferences = require('../consts/defaultPreferences.json');
+  const path = getPreferenceFilePath();
+  window.nodeAPI.fs.writeFileSync(path, JSON.stringify(defaultPreferences));
+}
+
+function getPreferences(path){
+  const file = window.nodeAPI.fs.readFileSync(path,'utf-8');
+  return JSON.parse(file);
+}
+
+function getPreferenceFilePath(){
+  const configDir = (window.nodeAPI.electron.app || window.nodeAPI.electron.remote.app).getPath('userData');
+  return `${configDir}/preferences.json`;
+}
